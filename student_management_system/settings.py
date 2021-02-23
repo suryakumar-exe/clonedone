b2d7ad0142c42fbffdf113cac52ff769ee67bbe4
suryakumar-exe <66153984+suryakumar-exe@users.noreply.github.com>
2021-02-22 23:25:10 -0800

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'student_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'student_management_app\templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
