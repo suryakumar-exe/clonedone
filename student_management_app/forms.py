8d81f0b22c2d2b9ad13ac1606bffa84356ad0dc1
suryakumar-exe <66153984+suryakumar-exe@users.noreply.github.com>
2021-02-23 20:10:36 -0800

    Update forms.py

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -13,7 +13,7 @@ class AddStudentForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
-    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control",'placeholder':'Along With Whatsapp Number'}))
 
     #For Displaying Courses
     try:
@@ -55,7 +55,7 @@ class EditStudentForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
     username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
-    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control",'placeholder':'Along With Whatsapp Number'}))
 
     #For Displaying Courses
     try:
