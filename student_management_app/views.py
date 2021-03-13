e4c5ff8e9b630a5fa77e750ec9f1e8fc4030cba1
suryakumar-exe <66153984+suryakumar-exe@users.noreply.github.com>
2021-03-13 00:44:03 -0800

    Update views.py

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from student_management_app.EmailBackEnd import EmailBackEnd
+from student_management_app.models import Students,CustomUser
 
 
 def home(request):
@@ -56,5 +57,37 @@ def get_user_details(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+def export_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="Students.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Students')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    #columns = ['First name', 'Last name', 'User name', 'Email address', 'Address', 'Phone Number', 'Gender','Course']
+    columns = ['Number']
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+    #rows = Students.objects.all().values_list(fn, 'last_name', 'username', 'email', 'address', 'number',
+                                              #'gender', 'course_name')
+    rows = Students.objects.all().values_list('number')
+
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
 
 
